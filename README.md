653d416f9ab874c6acfdf2c1fa0622713ff9a5fb
flobou <Florian.BOUDOT@publicis-modem.fr>
2013-06-10 12:19:01 +0200

    delete all

--- README.md
+++
@@ -1,4 +0,0 @@
-micromix
-========
-
-Theme Wordpress
--- navigation.php
+++
@@ -1,4 +0,0 @@
-<div class="navigation">
-	<p class="prev"><?php previous_post_link('&laquo; %link', 'Previous mix') ?></p>
-	<p class="next"><?php next_post_link('%link &raquo;', 'Next mix') ?></p>
-</div>
