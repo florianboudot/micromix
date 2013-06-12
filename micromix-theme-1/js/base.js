8db2ddc1ec16422ddc9061382e38aa61f7dfbb49
antsan2 <antoine.sanchez@publicis-modem.fr>
2013-06-12 18:23:50 +0200

    setup audio player

--- micromix-theme-1/js/base.js
+++ micromix-theme-1/js/base.js
@@ -1,12 +1,32 @@
 jQuery(document).ready(function() {
-
+    var $ = jQuery;
     /* SOUNDMANAGER SETUP */
+    var $pause = $('<div>');
     soundManager.setup({
-        url: theme_path + '/swf/', // where to find flash audio SWFs, as needed
+        url: '/wp-content/plugins/wpaudio-mp3-player/sm2/', // where to find flash audio SWFs, as needed
         // optional: prefer HTML5 over Flash for MP3/MP4
-        // preferFlash: false,
+        preferFlash: false,
         onready: function() {
-            //todo : only one player but many buttons in the website to control it
+            $('.wpaudio').each(function(i,o){
+                var audiosrc = this.href.replace('http://www.micromix.fr', '');
+                soundManager.createSound({
+                    url:audiosrc,
+                    id: this.parentNode.id,
+                    onplay:function(){$pause.css({background:'green'})},
+                    onpause:function(){$pause.css({background:'red'})}
+                })
+
+            }).bind('click', function(e){
+                    e.preventDefault();
+                    soundManager.pauseAll();
+                    soundManager.play(this.parentNode.id);
+
+                });
+            $pause.bind('click', function(){
+                soundManager.pauseAll();
+            });
+            $pause.css({position:'fixed',height:20,width:20,background:'red',top:40,right:40});
+            $('body').append($pause);
         }
     });
 
