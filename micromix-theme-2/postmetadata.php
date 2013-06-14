592365bdb2c91f4ec9ff55bc515092c8cef5a425
flobou <Florian.BOUDOT@publicis-modem.fr>
2013-06-14 16:10:05 +0200

    delete folder theme-2

--- micromix-theme-2/postmetadata.php
+++
@@ -1,8 +0,0 @@
-<p class="postmetadata">
-	<?php //the_tags('<span><strong>Artists : </strong> ', ', ', '.<br /></span>'); ?>
-	<span>
-		<strong>Style : </strong> <?php the_category(', ') ?>
-	</span>
-	
-</p>
-								
