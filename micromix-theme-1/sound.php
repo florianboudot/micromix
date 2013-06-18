89c2522775a8bd5076572a2df71d0090bbca847c
kazes <supakazes@hotmail.com>
2013-06-18 15:57:58 +0200

    [sound] clean the way you get mp3 link and mp3 file name

--- micromix-theme-1/sound.php
+++ micromix-theme-1/sound.php
@@ -1,14 +1,23 @@
-<?php 
-if (get_post_meta($post->ID, 'enclosure', true)) : 
-	
-	//enclosure tag is required by apple itunes podcast
-	$val = get_post_meta($post->ID, 'enclosure', false);
-	foreach ( (array) $val as $enc ) {
-		$enclosure = split("\n", $enc);
-	}
-	$enclosure = trim(htmlspecialchars($enclosure[0]));
-	$file = str_replace("http://www.micromix.fr/upload/", "", $enclosure);
-	$file = urldecode($file);
+<?php
+// current post ID
+$currentID = get_the_ID();
+
+$enclosure_customfield = get_post_meta($currentID, 'enclosure', true);
+
+// get mp3 link ("enclosure" tag is required by apple itunes)
+$mp3_link  = trim(htmlspecialchars($enclosure_customfield));
+
+// get file name (trim if there is prefix)
+$file_name = str_replace("http://www.micromix.fr/", "", $mp3_link);
+$file_name = str_replace("upload/", "", $file_name);
+
+// format url the same for everyone
+$mp3_link = "http://www.micromix.fr/upload/" . $file_name;
+$file_name = urldecode($file_name); // replace '%20' by blank spaces ' '
+
+// wordpress delete enclosure custom field if post update.
+// please check /wp-includes/functions.php and comment code starting with "foreach ( $pung as $link_test ) {"
+// more info : http://kevinjedwards.com/51/how-to-stop-wordpress-28-from-deleting-enclosures/
 ?>
 <div class="player">
     <div class="loaded"></div>
@@ -16,22 +25,15 @@ if (get_post_meta($post->ID, 'enclosure', true)) :
 </div>
 
 <div class="sound">
-    <p class="bt-player" id="bt-player-<?= the_ID(); ?>">
-        <a href="<?= $enclosure; ?>" class="wpaudio"><?= $file; ?></a>
+    <p class="bt-player" id="bt-player-<?= $currentID; ?>">
+        <a href="<?= $mp3_link; ?>" class="wpaudio"><?= $file_name; ?></a>
     </p>
 
-
-    <p class="stats" style="clear: both; display: none">
+    <p class="stats" style="clear: both; ">
         <em>
-            <?= print_download(get_the_ID(), false); ?> total plays
-            (<?= print_download(get_the_ID(), true); ?> plays this month)
-            <br>
+             <?= print_download($currentID, false); ?> total plays
+            (<?= print_download($currentID, true); ?> plays this month) <br>
             <?php
-            //afficher toutes les dl stats pour chaque mois
-
-            //récuper l'id du current post
-            $currentID = get_the_ID();
-
             //requete pour connaitre les mois renseignés
             $query = "SELECT download_count, dl_month FROM wp_downloadstats WHERE post_id = '".$currentID."' ORDER BY dl_month DESC";
             $line = $wpdb->get_results($query);
@@ -40,13 +42,10 @@ if (get_post_meta($post->ID, 'enclosure', true)) :
             foreach ($line as $row){
                 $dl_current_count = $row -> download_count;
                 $dl_current_month = $row -> dl_month;
-                echo($dl_current_month);
-                echo ' : ';
-                echo('('.$dl_current_count.')');
-                echo '<br>';
+
+                echo $dl_current_month . ' : ('.$dl_current_count.')<br>';
             }
             ?>
         </em>
     </p>
 </div><!-- .sound -->
-<?php endif; ?>
