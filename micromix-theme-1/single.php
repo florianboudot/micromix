0d81e18e77972707dfc371a5b560a2ecf5a27e27
romuleald <ckkoshi@gmail.com>
2014-09-05 15:49:15 +0200

    micromix #50 optimize page load
    * optimize ajax page loading by caching every single post (gain 200ms)

--- micromix-theme-1/single.php
+++ micromix-theme-1/single.php
@@ -1,34 +1,53 @@
 <?php
+$time0 = microtime();
+
 // HEADER
 if(!isajax()){
     get_header(); // <div id="column2"> is in header
 }
 echo '<div class="view" data-context="index">';
 
+
 // POSTS LOOP
 if (have_posts()) {
-    while (have_posts()) {
-        the_post();
+    $timepoststart = microtime();
+    $resultpost = wp_cache_get( 'single_post_' . get_the_ID());
+    if ( false === $resultpost ) {
 
-        // to get current id and highlight item in the menu (function allPostsByYear)
-        $_SESSION["article_id"] = get_the_ID();
+        ob_start();
 
-        // NAVIGATION
+        while (have_posts()) {
+            the_post();
+
+            // to get current id and highlight item in the menu (function allPostsByYear)
+            $_SESSION["article_id"] = get_the_ID();
+
+            // NAVIGATION
 //        if(!isajax()){
             include("navigation.php");
 //        }
 
-        // THE POST : title, image, playlist, player, etc.
-        include("the_post.php");
+            // THE POST : title, image, playlist, player, etc.
+            include("the_post.php");
 
-        // RELATED POSTS
-        if(function_exists('related_posts')) {
-            related_posts();
+            // RELATED POSTS
+            if(function_exists('related_posts')) {
+                related_posts();
+            }
+
+            // COMMENTS
+            comments_template();
         }
 
-        // COMMENTS
-        comments_template();
+        $resultpost = ob_get_contents();
+        ob_end_clean();
+
+
+        wp_cache_set( 'single_post_' . get_the_ID(), $resultpost );
     }
+    echo $resultpost;
+
+    $timepostend = microtime();
 }
 else { // NO POSTS FOUND
     echo "<p>Sorry, no posts matched your criteria.</p>";
@@ -39,4 +58,8 @@ echo '</div>';
 if(!isajax()){
     get_sidebar();
 }
+$timeend = microtime();
+
+echo "time total " . ($timepostend - $timepoststart) . "\n";
+echo "time total " . ($timeend - $time0) . "\n";
 ?>
