e7833b6e945483c8843208f27243f75bbdc96281
romuleald <ckkoshi@gmail.com>
2014-12-04 18:41:50 +0100

    mp3 fakename in comment

--- micromix-theme-1/functions.php
+++ micromix-theme-1/functions.php
@@ -3,6 +3,85 @@
 define( 'theme_path', get_bloginfo('template_url'));
 
 
+function convertAccentsAndSpecialToNormal($string) {
+	$table = array(
+		'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Ă'=>'A', 'Ā'=>'A', 'Ą'=>'A', 'Æ'=>'A', 'Ǽ'=>'A',
+		'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'ă'=>'a', 'ā'=>'a', 'ą'=>'a', 'æ'=>'a', 'ǽ'=>'a',
+
+		'Þ'=>'B', 'þ'=>'b', 'ß'=>'Ss',
+
+		'Ç'=>'C', 'Č'=>'C', 'Ć'=>'C', 'Ĉ'=>'C', 'Ċ'=>'C',
+		'ç'=>'c', 'č'=>'c', 'ć'=>'c', 'ĉ'=>'c', 'ċ'=>'c',
+
+		'Đ'=>'Dj', 'Ď'=>'D', 'Đ'=>'D',
+		'đ'=>'dj', 'ď'=>'d',
+
+		'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ĕ'=>'E', 'Ē'=>'E', 'Ę'=>'E', 'Ė'=>'E',
+		'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ĕ'=>'e', 'ē'=>'e', 'ę'=>'e', 'ė'=>'e',
+
+		'Ĝ'=>'G', 'Ğ'=>'G', 'Ġ'=>'G', 'Ģ'=>'G',
+		'ĝ'=>'g', 'ğ'=>'g', 'ġ'=>'g', 'ģ'=>'g',
+
+		'Ĥ'=>'H', 'Ħ'=>'H',
+		'ĥ'=>'h', 'ħ'=>'h',
+
+		'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'İ'=>'I', 'Ĩ'=>'I', 'Ī'=>'I', 'Ĭ'=>'I', 'Į'=>'I',
+		'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'į'=>'i', 'ĩ'=>'i', 'ī'=>'i', 'ĭ'=>'i', 'ı'=>'i',
+
+		'Ĵ'=>'J',
+		'ĵ'=>'j',
+
+		'Ķ'=>'K',
+		'ķ'=>'k', 'ĸ'=>'k',
+
+		'Ĺ'=>'L', 'Ļ'=>'L', 'Ľ'=>'L', 'Ŀ'=>'L', 'Ł'=>'L',
+		'ĺ'=>'l', 'ļ'=>'l', 'ľ'=>'l', 'ŀ'=>'l', 'ł'=>'l',
+
+		'Ñ'=>'N', 'Ń'=>'N', 'Ň'=>'N', 'Ņ'=>'N', 'Ŋ'=>'N',
+		'ñ'=>'n', 'ń'=>'n', 'ň'=>'n', 'ņ'=>'n', 'ŋ'=>'n', 'ŉ'=>'n',
+
+		'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ō'=>'O', 'Ŏ'=>'O', 'Ő'=>'O', 'Œ'=>'O',
+		'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ō'=>'o', 'ŏ'=>'o', 'ő'=>'o', 'œ'=>'o', 'ð'=>'o',
+
+		'Ŕ'=>'R', 'Ř'=>'R',
+		'ŕ'=>'r', 'ř'=>'r', 'ŗ'=>'r',
+
+		'Š'=>'S', 'Ŝ'=>'S', 'Ś'=>'S', 'Ş'=>'S',
+		'š'=>'s', 'ŝ'=>'s', 'ś'=>'s', 'ş'=>'s',
+
+		'Ŧ'=>'T', 'Ţ'=>'T', 'Ť'=>'T',
+		'ŧ'=>'t', 'ţ'=>'t', 'ť'=>'t',
+
+		'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ũ'=>'U', 'Ū'=>'U', 'Ŭ'=>'U', 'Ů'=>'U', 'Ű'=>'U', 'Ų'=>'U',
+		'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ũ'=>'u', 'ū'=>'u', 'ŭ'=>'u', 'ů'=>'u', 'ű'=>'u', 'ų'=>'u',
+
+		'Ŵ'=>'W', 'Ẁ'=>'W', 'Ẃ'=>'W', 'Ẅ'=>'W',
+		'ŵ'=>'w', 'ẁ'=>'w', 'ẃ'=>'w', 'ẅ'=>'w',
+
+		'Ý'=>'Y', 'Ÿ'=>'Y', 'Ŷ'=>'Y',
+		'ý'=>'y', 'ÿ'=>'y', 'ŷ'=>'y',
+
+		'Ž'=>'Z', 'Ź'=>'Z', 'Ż'=>'Z', 'Ž'=>'Z',
+		'ž'=>'z', 'ź'=>'z', 'ż'=>'z', 'ž'=>'z',
+
+		'“'=>'"', '”'=>'"', '‘'=>"'", '’'=>"'", '•'=>'-', '…'=>'...', '—'=>'-', '–'=>'-', '¿'=>'?', '¡'=>'!', '°'=>' degrees ',
+		'¼'=>' 1/4 ', '½'=>' 1/2 ', '¾'=>' 3/4 ', '⅓'=>' 1/3 ', '⅔'=>' 2/3 ', '⅛'=>' 1/8 ', '⅜'=>' 3/8 ', '⅝'=>' 5/8 ', '⅞'=>' 7/8 ',
+		'÷'=>' divided by ', '×'=>' times ', '±'=>' plus-minus ', '√'=>' square root ', '∞'=>' infinity ',
+		'≈'=>' almost equal to ', '≠'=>' not equal to ', '≡'=>' identical to ', '≤'=>' less than or equal to ', '≥'=>' greater than or equal to ',
+		'←'=>' left ', '→'=>' right ', '↑'=>' up ', '↓'=>' down ', '↔'=>' left and right ', '↕'=>' up and down ',
+		'℅'=>' care of ', '℮' => ' estimated ',
+		'Ω'=>' ohm ',
+		'♀'=>' female ', '♂'=>' male ',
+		'©'=>' Copyright ', '®'=>' Registered ', '™' =>' Trademark ',
+	);
+
+	$string = strtr($string, $table);
+	// Currency symbols: £¤¥€  - we dont bother with them for now
+	$string = preg_replace("/[^\x9\xA\xD\x20-\x7F]/u", "", $string);
+
+	return $string;
+}
+
 function isajax(){
     return isset($_GET['ajax']) || isset($_GET['kjax']) || isset($_GET['kajax']);
 }
@@ -172,6 +251,13 @@ function allPostsByYear() {
 	                array_push($post_mp3_array, $mp3_uri); // for javascript purposes
 	                array_push($post_url_array, $post_url);// for javascript purposes
 	                array_push($post_imagecover_array, $fat_image_src);// for javascript purposes
+
+//	                $fakemp3name = "Micromix " . $micromix_number . " - " . convertAccentsAndSpecialToNormal(trim( str_ireplace( "'", " ", $post_title ) )) . ".mp3";
+//	                if($mp3_uri != $fakemp3name){
+//		                echo "mp3 physique  | " . $mp3_uri . "\n";
+//		                echo "mp3 simulé | " . $fakemp3name . "\n";
+//		                echo "\n";
+//	                }
                     $totalposts--;
                 }
             }
