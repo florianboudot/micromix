d202dc39b0eebf4d03d9da166f674fc2be46f774
romuleald <ckkoshi@gmail.com>
2014-12-18 17:11:05 +0100

    JS error on search empty via $.fn.highlight

--- micromix-theme-1/js/app/base.js
+++ micromix-theme-1/js/app/base.js
@@ -7,9 +7,12 @@ var $ = jQuery;
 $.fn.extend({
     highlight: function(search, insensitive, hls_class){
         var regex = new RegExp("(<[^>]*>)|(\\b"+ search.replace(/([-.*+?^${}()|[\]\/\\])/g,"\\$1") +")", insensitive ? "ig" : "g");
-        return this.html(this.html().replace(regex, function(a, b, c){
-            return (a.charAt(0) == "<") ? a : "<span class=\""+ hls_class +"\">" + c + "</span>";
-        }));
+        var html = '';
+        if(this.html()){
+            html = this.html(this.html().replace(regex, function (a, b, c) {
+                return (a.charAt(0) == "<") ? a : "<span class=\"" + hls_class + "\">" + c + "</span>";
+            }));}
+        return html;
     }
 });
 
